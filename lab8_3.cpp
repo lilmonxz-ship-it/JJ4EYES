419d7d4f92b179ef7703f9892f15980a65690a68
lilmonxz-ship-it <lilmonxz@gmail.com>
2025-12-16 20:33:53 +0700

    Update lab8_3.cpp

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -11,3 +11,50 @@
 "Brook"
 "Jinbe"
 */
+
+#include<iostream>
+#include<iomanip>
+#include<string>
+using namespace std;
+
+int main()
+{
+    int A;
+    double B;
+    double H;
+    cout << "Enter your age: ";
+    cin >> A;
+        if (A <= 25 ){
+            cout <<  "Enter your height: ";   
+            cin >> H;   
+                if (H < 100){
+                    cout << "Your character = Chopper";
+                    }else if (H < 180){
+                        cout << "Your character = Usopp";
+                        }else{
+                            cout << "Enter your bounty: ";
+                            cin >> B;
+                            if (B <= 1100000000){
+                            cout << "Your character = Sanji";
+                            }
+                                else{
+                                cout << "Your character = Zoro";
+                                }
+            }
+        }
+                 else if (A > 25 and A <= 60){
+                    cout << "Enter your bounty: ";
+                    cin >> B;
+                        if (B <= 500000000){
+                            cout << "Your character = Franky";
+                            }
+                            else {
+                                cout << "Your character = Jinbe";
+                                }
+                    }
+                    else if (A > 60) {
+                        cout << "Your character = Brook";
+                        }
+    
+    
+}
